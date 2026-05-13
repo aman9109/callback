d49e1eec45922f2c7b40dce17b64f6705f3b3fdf
aman9109 <aman.a1@zerayatech.com>
2026-05-13 22:18:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/ztva/api/calendar/callback', (req, res) => {
-    // Get all query params Microsoft sends
     const queryString = new URLSearchParams(req.query).toString();
-    
-    // Redirect to your Azure HTTP backend
-    const azureUrl = `http://YOUR-AZURE-IP:8080/ztva/api/calendar/callback?${queryString}`;
-    
+    const azureUrl = `http://104.211.103.136:8080/ztva/api/calendar/callback?${queryString}`;
     console.log('Redirecting to:', azureUrl);
     res.redirect(azureUrl);
 });
